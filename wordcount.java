5c75dec00ea7e3e5d596cc1f3c272cd51858d213
kwanjas3 <kwanjas3@users.noreply.github.com>
2018-01-13 15:13:13 -0500

    Create wordcount.java

---
+++ wordcount.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class WordCount {
+    public static void main(String[] args) throws Exception{
+        File file = new File("/home/jk/IdeaProjects/WordCounter/src/text.txt");
+        Scanner scanner = new Scanner(file);
+
+        int words = 0;
+       while(scanner.hasNextLine()){
+           String line = scanner.nextLine();
+
+           words += (line.split(" ").length);
+       }
+       System.out.println(words);
+
+    }
+}
